3deeb86b37fd961f743046886d36c62da94d2a98
Mylyn-1122 <26zhengmylyn@gmail.com>
2020-09-16 19:46:19 -0700

    score

--- Pig.js
+++ Pig.js
@@ -5,6 +5,11 @@ class Pig extends BaseClass {
     this.Visiblity = 255;
   }
 
+  score(){
+    if (this.Visiblity < 0 && this.Visiblity > -1005){
+      score ++
+    }
+  }
  display(){
    //console.log(this.body.speed);
    if(this.body.speed < 3){
